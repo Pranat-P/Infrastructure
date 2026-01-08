fd98bfd82d31af2df0e4063ecb33747918a3c773
Pranat-P <pranat.pattankude@corazonkare.com>
2026-01-08 15:34:20 +0530

    updated

--- terraform/medvista-aws-ap-south-1/eks/variables.tf
+++ terraform/medvista-aws-ap-south-1/eks/variables.tf
@@ -22,25 +22,25 @@ variable "medvista_runner" {
   })
 }
 
-variable "eks_clusters" {
-  description = "List of EKS clusters and their configurations"
-  type = list(object({
-    cluster_name                             = string
-    cluster_version                          = string
-    cluster_endpoint_public_access           = bool
-    enable_cluster_creator_admin_permissions = bool
-    desired_size                             = number
-    min_size                                 = number
-    max_size                                 = number
-    instance_types                           = list(string)
-    capacity_type                            = string
-    labels                                   = map(string)
-    iam_role_additional_policies             = map(string)
-    service_account_name                     = string
-    tags                                     = optional(map(string))
-    cloudwatch_log_group_retention_in_days   = number
-  }))
-}
+# variable "eks_clusters" {
+#   description = "List of EKS clusters and their configurations"
+#   type = list(object({
+#     cluster_name                             = string
+#     cluster_version                          = string
+#     cluster_endpoint_public_access           = bool
+#     enable_cluster_creator_admin_permissions = bool
+#     desired_size                             = number
+#     min_size                                 = number
+#     max_size                                 = number
+#     instance_types                           = list(string)
+#     capacity_type                            = string
+#     labels                                   = map(string)
+#     iam_role_additional_policies             = map(string)
+#     service_account_name                     = string
+#     tags                                     = optional(map(string))
+#     cloudwatch_log_group_retention_in_days   = number
+#   }))
+# }
 
 variable "default_tags" {
   description = "Key-value pairs of default tags to apply to all resources."
--- terraform/medvista-aws-ap-south-1/s3/variables.tf
+++ terraform/medvista-aws-ap-south-1/s3/variables.tf
@@ -1,13 +1,13 @@
-variable "buckets" {
-  description = "A list of buckets to create."
-  type = list(object({
-    prefix     = string
-    name       = string
-    create_iam = bool
-  }))
-}
+# variable "buckets" {
+#   description = "A list of buckets to create."
+#   type = list(object({
+#     prefix     = string
+#     name       = string
+#     create_iam = bool
+#   }))
+# }
 
-variable "default_tags" {
-  description = "A map of tags to add to the buckets."
-  type        = map(string)
-}
+# variable "default_tags" {
+#   description = "A map of tags to add to the buckets."
+#   type        = map(string)
+# }
